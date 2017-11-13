f746ecd430a63b433e36fc0aacb76ab7e4bb3efc
Jacob <kinleyjd@gmail.com>
2017-11-12 20:51:46 -0500

    Initial site page
    First page, still testing github client

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Jacob Kinley</title>
+    <link rel="stylesheet" href="">
+    <style>
+
+    </style>
+    <script>
+
+    </script>
+</head>
+<body>
+    <h1>WIP Site, still testing</h1>
+
+</body>
+</html>
